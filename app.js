e93516136ca932e212fd0eaf9e4de463552c4167
OlloClark <ollo42@hotmail.co.uk>
2023-01-12 13:50:37 -0800

    adding active class JS CSS

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function openAccordion() {
 	const accordion = document.getElementsByClassName("accordion")
 	for (i = 0; i < accordion.length; i++) {
 		accordion[i].addEventListener("click", function() {
-	  
+		this.classList.toggle("active");
 		  let dropdown = this.nextElementSibling;
 		  if (dropdown.style.display === "block") {
 			dropdown.style.display = "none";
