2240ac23df43417d9eb144ecff81f1f664fb3c36
OlloClark <ollo42@hotmail.co.uk>
2023-01-13 12:34:52 -0800

    adding JS to rotate arrow

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ function openAccordion() {
 		accordion[i].addEventListener("click", function() {
 		this.classList.toggle("active");
 		  let dropdown = this.nextElementSibling;
+		  let arrow = this.firstElementChild;
 		  if (dropdown.style.display === "block") {
 			dropdown.style.display = "none";
 			this.style.borderBottom = "hsl(240, 5%, 91%) 1px solid";
+			arrow.style.transform = "none";
 		  } else {
 			dropdown.style.display = "block";
 			this.style.borderBottom = "none";
+			arrow.style.transform = "rotate(180deg)";
 		  }
 		});
 	  }
@@ -18,3 +21,13 @@ function openAccordion() {
 
 openAccordion()
 
+// function checkArrow() {
+// 	const arrow = document.getElementsByClassName("arrow");
+// 	for (i = 0; i < arrow.length; i++) {
+// 		arrow[i].addEventListener("click", function() {
+// 			console.log("clicked")
+// 	})
+
+// 	}}
+
+// 	checkArrow()
