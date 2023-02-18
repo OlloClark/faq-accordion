12f9152110e1b875109919e297b79a6850cfed6f
OlloClark <ollo42@hotmail.co.uk>
2023-02-17 19:30:10 -0800

    adding note for interviewers

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+
+
 function openAccordion() {
 	const accordion = document.getElementsByClassName("accordion")
 	for (i = 0; i < accordion.length; i++) {
@@ -6,6 +8,11 @@ function openAccordion() {
 		this.classList.toggle("active");
 		  let dropdown = this.nextElementSibling;
 		  let arrow = this.firstElementChild;
+//Welcome, Airbnb Team! 
+// I was particulary pleased to develop the dropdown functionality here. I have since been shown the
+// <details> element, but I feel like my solution has more opportunity for customization, as beyond
+// the toggling of visibility, I was able to animate the arrows, and could quickly adjust the
+// styling for any client.
 		  if (dropdown.style.display === "block") {
 			dropdown.style.display = "none";
 			this.style.borderBottom = "hsl(240, 5%, 91%) 1px solid";
